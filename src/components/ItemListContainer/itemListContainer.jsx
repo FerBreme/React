0a11c976001961d73d7021f1da4fb2e6126d25e8
Fernando Breme <ferchubreme123@gmail.com>
2022-08-29 22:57:40 -0300

    desafio 4

--- src/components/ItemListContainer/itemListContainer.jsx
+++ src/components/ItemListContainer/itemListContainer.jsx
@@ -1,8 +1,9 @@
-
+import Counter from "./itemCount";
 const ItemListContainer = (props) =>{
     return(
     <div>
         <h2 className="ItemContainer"> Bienvenido, soy {props.nombre}</h2>
+        <Counter />
     </div>
     )
     
